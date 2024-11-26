678da156865fdb84a6a294c0eca01506f499ce6d
Andres hdz <andreshermag@hotmail.com>
2024-11-26 17:10:33 -0600

    .env

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { createClient } from '@supabase/supabase-js'
 
-// Create a single supabase client for interacting with your database
-
-//SUPABASE_URL=https://uxxgdrzwgymksrcqkdcf.supabase.co
-//SUPABASE_API_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpX
-
-
-const supabase = createClient('SUPABASE_URL', 'SUPABASE_API_KEY', {
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_KEY,
+  {
   auth: {
     autoRefreshToken: false,
     persistSession: false
-  }
-})
+  },
+});
 
 const adminAuthClient = supabase.auth.admin
 
